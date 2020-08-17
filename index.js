487cdb32f7ac71310687cf0483424a792a10dbfd
I344427 <d.makkalikkal@sap.com>
2020-08-17 17:48:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function showFrameListWeb() {
+    document.getElementById("idFrame").style.visibility = "visible";
+    document.getElementById("idFrame").width ="98%";
+  }
+
+function showFrameListAndroid() {
+    document.getElementById("idFrame").width ="40%";
+  }
+
+ function onUpload() {
+    document.getElementById("idProgressBar").style.width ="100%";
+    document.getElementById("idViews").style.visibility = "visible";
+ } 
