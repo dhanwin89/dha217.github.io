7bd2b5ae9034694e7118ef33c2c8d76ce4ed717d
I344427 <d.makkalikkal@sap.com>
2020-08-19 15:52:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 function showFrameListWeb() {
+  document.getElementById("idFrame").src ="listReport.html";
     document.getElementById("idFrame").style.visibility = "visible";
     document.getElementById("idFrame").width ="98%";
   }
 
-function showFrameListAndroid() {
+function showFrameAndroid() {
     document.getElementById("idFrame").width ="40%";
   }
 
@@ -12,3 +13,19 @@ function showFrameListAndroid() {
     document.getElementById("idProgressBar").style.width ="100%";
     document.getElementById("idViews").style.visibility = "visible";
  } 
+
+ function showFrameAnalytics() {
+  document.getElementById("idFrame").src ="sm.html";
+  document.getElementById("idFrame").style.visibility = "visible";
+  document.getElementById("idFrame").width ="98%";
+
+ }
+  function showFrameWeb() {
+    document.getElementById("idFrame").width ="98%";
+  }
+
+  function showFrameKanban() {
+    document.getElementById("idFrame").src ="page.html";
+    document.getElementById("idFrame").style.visibility = "visible";
+    document.getElementById("idFrame").width ="98%";
+  }
